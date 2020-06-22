6dffb336d52f4eece80301663fd4a295754f4191
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-22 11:11:26 +0530

    php artisan make:model Role -m

--- app/Role.php
+++ app/Role.php
@@ -3,8 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    public $transformer = RoleTransformer::class;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role', 'client_details'
+    ];
 }
