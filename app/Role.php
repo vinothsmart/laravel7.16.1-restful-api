08f4f302bd6b37b5f847c1a0406a61b9bb2b9607
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-22 11:08:58 +0530

    Created Model and Migration for Role

--- app/Role.php
+++ app/Role.php
@@ -3,20 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use SoftDeletes;
-
-    protected $dates = ['deleted_at'];
-    
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'role', 'client_details'
-    ];
+    //
 }
