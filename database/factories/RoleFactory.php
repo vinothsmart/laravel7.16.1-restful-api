cdab316531d64a25f4c68744cd3fd178ec6a6f64
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-23 09:55:18 +0530

    Role Factory up

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -5,8 +5,10 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Role::class, function (Faker $faker) {
     return [
-        //
+        'role' => $faker->name,
+        'created_at' => now(),
+        'updated_at' => now(),
     ];
 });
