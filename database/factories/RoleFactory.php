a610329588b49647b8c9a23ed18711005661f8db
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-23 10:00:46 +0530

    Role Factory up

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Role;
 use Faker\Generator as Faker;
 
 $factory->define(Role::class, function (Faker $faker) {
