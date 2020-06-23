8debf9a4065f76cd49a6d1650a9b10c6e6210658
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-23 14:47:13 +0530

    dynamic users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,5 +13,8 @@ class UserSeeder extends Seeder
     public function run()
     {
         factory(User::class)->create(['name' => 'Vino', 'email' => 'vino@ab.com', 'password' => '$2y$10$1KeHVDk4jl1LU7psksi9iet369iU7eli4fshl/zXcIxQqKCLMTZSW','image' => 'default.jpg', 'verified' => 1, 'admin' => User::ADMIN_USER, 'verification_token' => null]);
+
+        $usersQunatity = 1000;
+        factory(User::class, $usersQunatity)->create();
     }
 }
