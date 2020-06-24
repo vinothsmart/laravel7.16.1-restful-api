9d904c055100bfaca2e3a6af9c774bcd3fcb03b2
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-24 15:51:28 +0530

    ROles based users list

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,22 +11,24 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
 /**
-* Roles
-*/
+ * Roles
+ */
 
 Route::resource('roles', 'Role\RoleController', ['except' => ['create', 'edit']]);
 
-Route::name('rolesList')->get('rolesList', 'Role\RoleController@list'); 
+Route::name('rolesList')->get('rolesList', 'Role\RoleController@list');
+
+Route::resource('roles.users', 'Role\RoleUserController', ['only' => ['index']]);
 
 /**
-* Users
-*/
+ * Users
+ */
 
-Route::resource('users', 'User\UserController',['except' => ['create', 'edit']]);
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
