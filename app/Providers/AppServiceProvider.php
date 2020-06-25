5955e77c4269f0e578da405e8003e1cee3e18b0b
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-25 16:29:55 +0530

    sending mail auto tigger mail when updating user email only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Mail\UserCreated;
+use App\Mail\UserMailChanged;
 use App\User;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
@@ -30,6 +31,12 @@ class AppServiceProvider extends ServiceProvider
             retry(5, function () use ($user) {
                 Mail::to($user)->send(new UserCreated($user));
             }, 100);
+        });
+
+        User::updated(function ($user) {
+            if ($user->isDirty('email')) {
+                Mail::to($user)->send(new UserMailChanged($user));
+            }
         });
     }
 }
