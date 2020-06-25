4732482358e8ce88cbd869ac6cd8e694c3a442c0
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-25 16:04:54 +0530

    sending mail auto tigger mail when adding user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Mail\UserCreated;
+use App\User;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::created(function ($user) {
+            retry(5, function () use ($user) {
+                Mail::to($user)->send(new UserCreated($user));
+            }, 100);
+        });
     }
 }
