5a32af719223764a96706e33e9d538eaedfc937f
vinothsmart <s.vinothkanna90@gmail.com>
2020-09-28 15:10:20 +0530

    psql config md

--- config/database.php
+++ config/database.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Str;
 
 // Configuration for cleardb
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 
 return [
 
@@ -25,7 +25,9 @@ return [
 
     // 'default' => env('DB_CONNECTION', 'mysql'),
 
-    'default' => env('DB_CONNECTION', 'your_heroku_mysql_connection'),
+    // 'default' => env('DB_CONNECTION', 'your_heroku_mysql_connection'),
+
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -45,16 +47,16 @@ return [
 
     'connections' => [
 
-        'your_heroku_mysql_connection' => array(
-            'driver' => 'mysql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-        ),
+        // 'your_heroku_mysql_connection' => array(
+        //     'driver' => 'mysql',
+        //     'host' => $host,
+        //     'database' => $database,
+        //     'username' => $username,
+        //     'password' => $password,
+        //     'charset' => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix' => '',
+        // ),
 
         'sqlite' => [
             'driver' => 'sqlite',
